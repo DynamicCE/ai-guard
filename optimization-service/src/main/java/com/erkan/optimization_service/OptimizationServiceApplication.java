486848d0e213985b7333f90e917a7fb1f24ede40
Adınız <erkanmurat0632@gmail.com>
2024-12-09 12:42:27 +0300

    project started

---
+++ optimization-service/src/main/java/com/erkan/optimization_service/OptimizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erkan.optimization_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizationServiceApplication.class, args);
+	}
+
+}
